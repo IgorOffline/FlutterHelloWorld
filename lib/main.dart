7868f48955498d6f63574d5e46432ab6d13af135
Igor Durbek <igor@igordurbek.com>
2023-01-28 18:46:23 +0100

    cleaned up GridView

--- lib/main.dart
+++ lib/main.dart
@@ -63,158 +63,31 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       BoardSquare(BoardLetter.a, BoardNumber.n1, Piece.king, PieceColor.white);
   BoardSquare b1 =
       BoardSquare(BoardLetter.b, BoardNumber.n1, Piece.king, PieceColor.black);
-  double commonSize = 50;
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Expanded(
-        flex: 5,
-        child: Row(
-          children: <Widget>[
-            Expanded(
-                flex: 5,
-                child: Container(
-                    color: Color(0xFFD32F2F),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        DragTarget<BoardSquare>(
-                          builder: (
-                            BuildContext context,
-                            List<dynamic> accepted,
-                            List<dynamic> rejected,
-                          ) {
-                            return Container(
-                              height: 100.0,
-                              width: 100.0,
-                              color: Colors.cyan,
-                              child: Center(
-                                child: Draggable<BoardSquare>(
-                                  // Data is the value this Draggable stores.
-                                  data: a1,
-                                  feedback: Container(
-                                    color: Colors.transparent,
-                                    height: 100,
-                                    width: 100,
-                                    child: Icon(Icons.directions_run),
-                                  ),
-                                  childWhenDragging: Container(
-                                    height: 100.0,
-                                    width: 100.0,
-                                    color: Colors.transparent,
-                                    child: Center(
-                                      child: Text(
-                                          'Child When Dragging ${a1.pieceColor}'),
-                                    ),
-                                  ),
-                                  child: Container(
-                                    height: 100.0,
-                                    width: 100.0,
-                                    color: Colors.transparent,
-                                    child: Center(
-                                      child: Text('Draggable ${a1.pieceColor}'),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            );
-                          },
-                          onAccept: (BoardSquare data) {
-                            setState(() {
-                              var tempPieceColor = a1.pieceColor;
-                              a1.pieceColor = data.pieceColor;
-                              data.pieceColor = tempPieceColor;
-                            });
-                          },
-                        ),
-                        DragTarget<BoardSquare>(
-                          builder: (
-                            BuildContext context,
-                            List<dynamic> accepted,
-                            List<dynamic> rejected,
-                          ) {
-                            return Container(
-                              height: 100.0,
-                              width: 100.0,
-                              color: Colors.cyan,
-                              child: Center(
-                                child: Draggable<BoardSquare>(
-                                  // Data is the value this Draggable stores.
-                                  data: b1,
-                                  feedback: Container(
-                                    color: Colors.transparent,
-                                    height: 100,
-                                    width: 100,
-                                    child: Icon(Icons.directions_run),
-                                  ),
-                                  childWhenDragging: Container(
-                                    height: 100.0,
-                                    width: 100.0,
-                                    color: Colors.transparent,
-                                    child: Center(
-                                      child: Text(
-                                          'Child When Dragging ${b1.pieceColor}'),
-                                    ),
-                                  ),
-                                  child: Container(
-                                    height: 100.0,
-                                    width: 100.0,
-                                    color: Colors.transparent,
-                                    child: Center(
-                                      child: Text('Draggable ${b1.pieceColor}'),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            );
-                          },
-                          onAccept: (BoardSquare data) {
-                            setState(() {
-                              var tempPieceColor = b1.pieceColor;
-                              b1.pieceColor = data.pieceColor;
-                              data.pieceColor = tempPieceColor;
-                            });
-                          },
-                        ),
-                      ],
-                    ))),
-            Expanded(
-                flex: 5,
-                child: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxHeight: 250,
-                      maxWidth: 250,
-                    ),
-                    child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 8,
-                      ),
-                      itemBuilder: _itemBuilder,
-                      itemCount: 8 * 8,
-                      physics: const NeverScrollableScrollPhysics(),
-                    )))
-          ],
-        ),
-      ),
-      Expanded(
-        flex: 5,
-        child: Row(
-          children: <Widget>[
-            Expanded(
-                flex: 5,
-                child: Container(
-                  color: Color(0xFFFBC02D),
-                )),
-            Expanded(
-                flex: 5,
-                child: Container(
-                  color: Color(0xFF512DA8),
-                )),
-          ],
-        ),
-      )
-    ]);
+    return SingleChildScrollView(
+        child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+          Container(
+            width: 265,
+            height: 265,
+            margin:
+                const EdgeInsets.only(left: 10, top: 10, right: 0, bottom: 0),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 2.0)),
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 8,
+              ),
+              itemBuilder: _itemBuilder,
+              itemCount: 8 * 8,
+              physics: const NeverScrollableScrollPhysics(),
+            ),
+          )
+        ]));
   }
 
   Widget _itemBuilder(BuildContext context, int index) {
